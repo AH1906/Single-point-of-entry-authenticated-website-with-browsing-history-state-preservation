57ec7ef1c41a69bd9a689885269d149f110864ae
Abdul Haseeb <110911712+AH1906@users.noreply.github.com>
2026-09-24 19:37:23 +0100

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,5 @@
+<?php
+$headTitle = 'Page 1';
+$viewHeading = htmlHeading('Page 1', 2);
+$content = htmlParagraph('Page 1 Content');
+?>
---
+++ page2.php
@@ -0,0 +1,5 @@
+<?php
+$headTitle = 'Page 2';
+$viewHeading = htmlHeading('Page 2', 2);
+$content = htmlParagraph('Page 2 Content');
+?>
