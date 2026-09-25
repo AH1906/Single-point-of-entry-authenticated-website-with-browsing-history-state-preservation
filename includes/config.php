f9a625979b34fea8c07684c91b6523e07b27b8b2
Abdul Haseeb <110911712+AH1906@users.noreply.github.com>
2026-09-25 01:54:06 +0100

    Update database connection configuration placeholders

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'mysqlsrv.dcs.bbk.ac.uk';
-$db   = 'abutt20db'; 	#your MySQL database name goes here
-$user = 'abutt20';		#your ITS loginID
-$pass = 'bbkmysql';		#password
+$host = 'your_host_here';
+$db = 'your_db_name';
+$user = 'your_username';
+$pass = 'your_password';
 $charset = 'utf8mb4';	#DO NOT CHANGE
 #do not alter anything below this line
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
